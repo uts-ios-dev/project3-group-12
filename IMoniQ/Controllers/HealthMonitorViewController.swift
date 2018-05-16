59096c393c7d00dbe3649457d41201354cc9d6e2
DezeG <gromovdeze@yandex.ru>
2018-05-17 09:21:01 +1000

    Update HealthMonitorViewController.swift

--- IMoniQ/Controllers/HealthMonitorViewController.swift
+++ IMoniQ/Controllers/HealthMonitorViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class HealthMonitorViewController: UIViewController {
 
+    @IBOutlet weak var verticalSlider: UISlider! {
+        didSet{
+            verticalSlider.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +26,39 @@ class HealthMonitorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func verticalSliderChanged(_ sender: UISlider) {
+        sliderValueOutlet.text = String(sender.value)
+        if(sender.value > 5.5) {
+            superSad.transform = CGAffineTransform(scaleX: CGFloat(1.5 - abs(6.0 - sender.value)), y: CGFloat(1.5 - abs(6.0 - sender.value)))
+        }
+        if(sender.value > 4.5 && sender.value < 5.5) {
+            sad.transform = CGAffineTransform(scaleX: CGFloat(1.5 - abs(5.0 - sender.value)), y: CGFloat(1.5 - abs(5.0 - sender.value)))
+        }
+        if(sender.value > 3.5 && sender.value < 4.5) {
+            littleSad.transform = CGAffineTransform(scaleX: CGFloat(1.5 - abs(4.0 - sender.value)), y: CGFloat(1.5 - abs(4.0 - sender.value)))
+        }
 
+        if(sender.value > 2.5 && sender.value < 3.5) {
+            littleHappy.transform = CGAffineTransform(scaleX: CGFloat(1.5 - abs(3.0 - sender.value)), y: CGFloat(1.5 - abs(3.0 - sender.value)))
+        }
+        if(sender.value > 1.5 && sender.value < 2.5) {
+            happy.transform = CGAffineTransform(scaleX: CGFloat(1.5 - abs(2.0 - sender.value)), y: CGFloat(1.5 - abs(2.0 - sender.value)))
+        }
+
+        if(sender.value > 0.5 && sender.value < 1.5) {
+            superHappy.transform = CGAffineTransform(scaleX: CGFloat(1.5 - abs(1.0 - sender.value)), y: CGFloat(1.5 - abs(1.0 - sender.value)))
+        }
+
+    }
+    
+    @IBOutlet weak var sliderValueOutlet: UILabel!
+    @IBOutlet weak var superHappy: UIImageView!
+    @IBOutlet weak var superSad: UIImageView!
+    @IBOutlet weak var happy: UIImageView!
+    @IBOutlet weak var littleHappy: UIImageView!
+    @IBOutlet weak var littleSad: UIImageView!
+    @IBOutlet weak var sad: UIImageView!
+    
     /*
     // MARK: - Navigation
 
