51c4ca44b3556452e14a7e364444041ad169cd33
Andrey Maslennikov <cheze92@gmail.com>
2018-05-20 19:01:58 +1000

    updated all  Changes to be committed: 	modified:   IMoniQ.xcodeproj/project.pbxproj 	modified:   IMoniQ/Controllers/HealthMonitorDetailViewController.swift 	new file:   IMoniQ/Controllers/userPainv2.swift

--- IMoniQ/Controllers/HealthMonitorDetailViewController.swift
+++ IMoniQ/Controllers/HealthMonitorDetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Charts
 
 class HealthMonitorDetailViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         barChartView.leftAxis.drawGridLinesEnabled = false
