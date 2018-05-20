73f3b28e47d4aaffb536550587226f7551304440
Andrei Maslennikov <cheze92@gmail.com>
2018-05-20 17:06:06 +1000

    sleepcontroller

--- IMoniQ/Controllers/SleepMonitorViewController.swift
+++ IMoniQ/Controllers/SleepMonitorViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import HealthKit
 
 class SleepMonitorViewController: UIViewController {
 
+    var startTime = TimeInterval()
+    var timer:Timer = Timer()
+    var endTime: NSDate!
+    var alarmTime: NSDate!
+    
+    let healthStore = HKHealthStore()
+    
+    @IBOutlet weak var buttonStart: UIButton!
+    @IBOutlet weak var buttonStop: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let typestoRead = Set([
+            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
+            ])
+        
+        let typestoShare = Set([
+            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
+            ])
+        
+        self.healthStore.requestAuthorization(toShare: typestoShare, read: typestoRead) { (success, error) -> Void in
+            if success == false {
+                NSLog(" Display not allowed")
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +43,99 @@ class SleepMonitorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func startSleep(_ sender: UIButton) {
+        alarmTime = NSDate()
+        if (!timer.isValid) {
+            let aSelector : Selector = #selector(SleepMonitorViewController.updateTime)
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate
+        }
     }
-    */
-
+    @IBAction func stopSleep(_ sender: Any) {
+        endTime = NSDate()
+        saveSleepAnalysis()
+        retrieveSleepAnalysis()
+        timer.invalidate()
+    }
+    @objc func updateTime() {
+    }
+    
+    func saveSleepAnalysis() {
+        
+        // alarmTime and endTime are NSDate objects
+        if let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
+            
+            // we create our new object we want to push in Health app
+            let object = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.inBed.rawValue, start: self.alarmTime as Date as Date, end: self.endTime as Date)
+            
+            // at the end, we save it
+            healthStore.save(object, withCompletion: { (success, error) -> Void in
+                
+                if error != nil {
+                    // something happened
+                    return
+                }
+                
+                if success {
+                    print("My new data was saved in HealthKit")
+                    
+                } else {
+                    // something happened again
+                }
+                
+            })
+            
+            
+            let object2 = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.asleep.rawValue, start: self.alarmTime as Date, end: self.endTime as Date)
+            
+            healthStore.save(object2, withCompletion: { (success, error) -> Void in
+                if error != nil {
+                    // something happened
+                    return
+                }
+                
+                if success {
+                    print("My new data (2) was saved in HealthKit")
+                } else {
+                    // something happened again
+                }
+                
+            })
+            
+        }
+        
+    }
+    
+    func retrieveSleepAnalysis() {
+        
+        // first, we define the object type we want
+        if let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
+            
+            // Use a sortDescriptor to get the recent data first
+            let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+            
+            // we create our query with a block completion to execute
+            let query = HKSampleQuery(sampleType: sleepType, predicate: nil, limit: 30, sortDescriptors: [sortDescriptor]) { (query, tmpResult, error) -> Void in
+                
+                if error != nil {
+                    
+                    // something happened
+                    return
+                    
+                }
+                
+                if let result = tmpResult {
+                    
+                    // do something with my data
+                    for item in result {
+                        if let sample = item as? HKCategorySample {
+                            let value = (sample.value == HKCategoryValueSleepAnalysis.inBed.rawValue) ? "InBed" : "Asleep"
+                            print("Healthkit sleep: \(sample.startDate) \(sample.endDate) - value: \(value)")
+                        }
+                    }
+                }
+            }
+            healthStore.execute(query)
+        }
+    }
 }
